514dc4f13df1f74715fe73b31e8e784d2a039841
Marcelo Henrique <marcelo123.mh91@gmail.com>
2023-12-10 11:54:39 -0300

    lista_alocaçao_dinamica

--- ALOCAÇÃO DINÂMICA/lista_questao_2.c
+++ ALOCAÇÃO DINÂMICA/lista_questao_2.c
@@ -2,35 +2,30 @@
 #include <stdlib.h>
 
 int main() {
-    int numQuestoes;
+    int numeroquestoes;
     
     printf("Digite o número de questões: ");
-    scanf("%d", &numQuestoes);
+    scanf("%d", &numeroquestoes);
 
-    char *gabarito = (char *)malloc(numQuestoes * sizeof(char));
-    char **respostasAlunos = (char **)malloc(10 * sizeof(char *));
-    
-    if (gabarito == NULL || respostasAlunos == NULL) {
-        printf("Erro na alocação de memória.\n");
-        exit(EXIT_FAILURE);
-    }
+    char *gabarito = (char *)malloc(numeroquestoes * sizeof(char));
+    char **respostas = (char **)malloc(10 * sizeof(char *));
 
     printf("Digite o gabarito da prova (ex: ABCDE...): ");
     scanf("%s", gabarito);
 
     for (int i = 0; i < 10; i++) {
-        respostasAlunos[i] = (char *)malloc(numQuestoes * sizeof(char));
-        printf("Digite as respostas do aluno %d (ex: ABCDE...): ", i + 1);
-        scanf("%s", respostasAlunos[i]);
+        respostas[i] = (char *)malloc(numeroquestoes * sizeof(char));
+        printf("Digite as respostas do aluno %d (ex: ABCDE): ", i + 1);
+        scanf("%s", respostas[i]);
     }
 
     printf("\nNotas dos alunos:\n");
     int aprovados = 0;
     for (int i = 0; i < 10; i++) {
         int nota = 0;
-        for (int j = 0; j < numQuestoes; j++) {
-            if (respostasAlunos[i][j] == gabarito[j]) {
-                nota += 10 / numQuestoes;
+        for (int j = 0; j < numeroquestoes; j++) {
+            if (respostas[i][j] == gabarito[j]) {
+                nota += 10 / numeroquestoes;
             }
         }
 
@@ -41,13 +36,13 @@ int main() {
         }
     }
 
-    printf("\nPorcentagem de aprovação: %.2f%%\n", (aprovados / 10.0) * 100);
+    printf("\nPorcentagem de aprovados: %.2f%%\n", (aprovados / 10.0) * 100);
 
     free(gabarito);
     for (int i = 0; i < 10; i++) {
-        free(respostasAlunos[i]);
+        free(respostas[i]);
     }
-    free(respostasAlunos);
+    free(respostas);
 
     return 0;
 }
